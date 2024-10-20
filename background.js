2e78d5c217298b932d727b06daaccd07e03572ae
FracturedStar-dog <coolman2472@gmail.com>
2024-10-21 01:51:32 +0300

    Update background.js
    I fucked up but its fixed now :D

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url) {
-      if (tab.url.includes("x.com") || tab.url.includes("twitter.com")) {
-        chrome.tabs.update(tabId, { url: "https://bsky.app/" });
-      }
+  if (changeInfo.status === 'complete' && tab.url) {
+    const url = new URL(tab.url);
+    
+    if ((url.hostname === 'x.com' || url.hostname === 'twitter.com') && (url.pathname === '/' || url.pathname === '')) {
+      chrome.tabs.update(tabId, { url: "https://bsky.app/" });
     }
-  });
-  
+  }
+});
